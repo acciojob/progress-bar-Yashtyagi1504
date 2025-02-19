d5a4a39965c8feedb4691218ad655ec2f9a20e70
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-02-20 02:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
+let circles = document.querySelectorAll(".circle");
+let bars = document.querySelectorAll(".bar");
+
+let index = 0;
+nextBtn.addEventListener("click",()=>{
+	prevBtn.disabled = false;
+	bars[index].classList.add("active");
+	circles[index+1].classList.add("active");
+	index++;
+	if(index==4){
+		nextBtn.disabled = true;
+	}
+});
+
+prevBtn.addEventListener("click",()=>{
+	bars[index-1].classList.remove("active")
+	circles[index].classList.remove("active");
+	index--;
+	nextBtn.disabled = false;
+	if(index==0){
+		prevBtn.disabled = true;
+	}
+})
